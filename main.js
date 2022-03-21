16a7e419acbce3c6939d6cd196119bb560ec84a8
Sviatoslove <svyatpopov86@gmail.com>
2022-03-21 21:48:35 +0200

    start_to_JS

---
+++ main.js
@@ -0,0 +1,37 @@
+//Task1
+var firstName = 'Sviatoslav',
+  lastName = 'Popov',
+  age = '35 years old',
+  maritalStatus = 'unmarried',
+  child = 'no children';
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(maritalStatus);
+console.log(child);
+console.log(typeof maritalStatus);
+console.log(typeof child);
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof age);
+
+//Task2
+var rectangleHeight = 23,
+ rectangleWidth = 10,
+ areaRectangle = (rectangleHeight * rectangleWidth);
+console.log(areaRectangle);
+
+//Task3
+var sBaseСylinder = 4, 
+  heightCylinder = 10, 
+  vCylinder = (sBaseСylinder * heightCylinder);
+console.log(vCylinder);
+
+//Task4
+console.log(Infinity - "1")// Infinity - "1"//Infinity
+console.log("42" + 42)//"42" + 42//4242
+console.log(2 + "1 1")//2 + "1 1"//21 1
+console.log(99 + 101)//99 + 101//200
+console.log("1" - "1")//"1" - "1"//0
+console.log("Result: " + 10/2)//"Result: " + 10/2//Result: 5
+console.log(3 + " bananas " + 2 + " apples ")//3 + " bananas " + 2 + " apples "//3 bananas 2 apples
